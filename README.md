bbde6a14a3ea2b20f330e289de865d56128704b5
Shawn Rutledge <s@ecloud.org>
2026-03-19 12:27:40 +0100

    Behold the turtle
    More or less a copy of the ROS Kilted turtlesim example,
    modified for Qt 6 and to try not to depend on ament.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Dogzilla daemon
+
+An attempt to replace `DOGZILLA/app_dogzilla/app_dogzilla.py` on the 
+[Dogzilla S2](https://category.yahboom.net/products/dogzilla-s1?variant=46390953640252)
+with a Qt-based daemon, using ROS as one means of remote control / monitoring,
+in addition to the game controller.
+
+So far this is meant to be checked out under a ROS workspace, e.g. 
+`~/ros2_ws/src/dogzilla`
+
+```
+$ source /opt/ros/kilted/setup.bash
+$ cd ~/ros2_ws
+$ colcon _build_ --packages-select dogzilla --cmake-args -DCMAKE_BUILD_TYPE=Debug
+$ cd build/dogzilla
+$ ./dogzillad
+```
+The linker arguments are enormous, so linking takes a couple of minutes.
+
+For the moment it's a paralyzed dog that thinks it's a turtle.  Since the
+"daemon" is graphical, you can use `ssh -X` to connect.
+
--- src/dogzillad.cpp
+++ src/dogzillad.cpp
@@ -1,6 +1,39 @@
-#include <cstdio>
+#include <QApplication>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char ** argv)
+#include <rclcpp/rclcpp.hpp>
+
+#include "turtlesim/turtle_frame.hpp"
+
+class DogzillaApp : public QApplication
 {
-  printf("hello world dogzilla package\n");
+public:
+  rclcpp::Node::SharedPtr nh_;
+
+  explicit DogzillaApp(int & argc, char ** argv)
+  : QApplication(argc, argv)
+  {
+    rclcpp::init(argc, argv);
+    nh_ = rclcpp::Node::make_shared("turtlesim");
+  }
+
+  ~DogzillaApp()
+  {
+    rclcpp::shutdown();
+  }
+
+  int exec()
+  {
+	  // TODO create OLED framebuffer UI instead
+
+    turtlesim::TurtleFrame frame(nh_);
+    frame.show();
+
+    return QApplication::exec();
+  }
+};
+
+int main(int argc, char ** argv)
+{
+  DogzillaApp app(argc, argv);
+  return app.exec();
 }
