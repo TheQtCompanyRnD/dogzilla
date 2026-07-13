dfbe44d60f39dd41800212b9c559b72654f4a7c7
Shawn Rutledge <s@ecloud.org>
2026-07-13 22:30:58 +0200

    dogzillad: Run main.qml installed in AppDataLocation if possible

--- dogzillad/src/dogzillad.cpp
+++ dogzillad/src/dogzillad.cpp
@@ -1,5 +1,7 @@
 #include <QCoreApplication>
 #include <QQmlApplicationEngine>
+#include <QStandardPaths>
+#include <QUrl>
 #include <QtRos2Core/qros2context.h>
 #include "controller.h"
 
@@ -7,15 +9,27 @@ int main(int argc, char **argv)
 {
     QRos2Context::init(argc, argv);
     QCoreApplication app(argc, argv);
+    // set the AppDataLocation suffix
+    QCoreApplication::setApplicationName(QStringLiteral("dogzillad"));
+
+    // main.qml is an editable script rather than a compiled-in resource.
+    // AppDataLocation searches ~/.local/share/dogzillad then the installed
+    // /usr/share/dogzillad/main.qml. Fall back to ./qml/main.qml for running
+    // straight out of the build/source tree during development.
+    QString qmlPath =
+            QStandardPaths::locate(QStandardPaths::AppDataLocation, QStringLiteral("main.qml"));
+    if (qmlPath.isEmpty())
+        qmlPath = QStringLiteral("qml/main.qml");
+
     QQmlApplicationEngine engine;
-    qDebug() << "import paths" << engine.importPathList();
+    qDebug() << "loading" << qmlPath << "import paths" << engine.importPathList();
     QObject::connect(
             &engine,
             &QQmlApplicationEngine::objectCreationFailed,
             &app,
             []() { QCoreApplication::exit(-1); },
             Qt::QueuedConnection);
-    engine.load("qml/main.qml");
+    engine.load(QUrl::fromLocalFile(qmlPath));
     if (engine.rootObjects().isEmpty())
         return -1;
     return QCoreApplication::exec();
